c0728d309a85046779aa557525f575c254e4bdcc
kondo-akihiro-git <2000akisan@gmail.com>
2025-02-07 10:03:53 +0900

    画面表示をテーブル形式に修正

--- notion-table-viewer-frontend/src/components/NotionTable.vue
+++ notion-table-viewer-frontend/src/components/NotionTable.vue
@@ -1,13 +1,38 @@
 <template>
     <div>
-        <h1>Notion テーブルのデータ</h1>
-        <ul v-if="data.length">
-            <li v-for="(item, index) in data" :key="index">
-                <span v-if="item.properties['テスト名前'].title && item.properties['テスト名前'].title.length">
-                    {{ item.properties['テスト名前'].title[0].text.content }}
-                </span>
-            </li>
-        </ul>
+        <h1>Notion データベースの情報</h1>
+
+        <!-- テーブル名とビュー名 -->
+        <h2>データベース: テストテーブル</h2>
+        <h3>ビュー: 全データ</h3>
+
+        <!-- データがある場合にテーブルを表示 -->
+        <table v-if="data.length">
+            <thead>
+                <tr>
+                    <th v-for="(value, key) in data[0].properties" :key="key">
+                        {{ key }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in data" :key="index">
+                    <td v-for="(value, key) in item.properties" :key="key">
+                        <!-- Rich textプロパティやタイトルなどの内容を取り出す処理 -->
+                        <span v-if="value.type === 'rich_text'">
+                            <span v-for="(richText, idx) in value.rich_text" :key="idx">
+                                {{ richText.plain_text }}
+                            </span>
+                        </span>
+
+                        <span v-if="value.type === 'title'">
+                            {{ value.title[0]?.plain_text || 'タイトルなし' }}
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
         <p v-else>データを読み込んでいます...</p>
     </div>
 </template>
@@ -40,5 +65,22 @@ export default {
 <style scoped>
 h1 {
     color: #42b983;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+th,
+td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+    background-color: #f4f4f4;
 }
 </style>
